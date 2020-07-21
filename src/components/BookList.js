e168df115b85ec08d7525d1ac89485e1fb343561
Ankush Thakur <ankush.thakur53@gmail.com>
2020-07-21 13:52:11 +0530

    Apply theme context to Navbar and BookList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
+  static contextType = ThemeContext;
+
   render() { 
+    const {isLightTheme, dark, light} = this.context;
+    const theme = isLightTheme ? light : dark;
+
     return (  
-      <div className="book-list">
+      <div className="book-list" style={{background: theme.bg, color: theme.text}}>
         <ul>
-          <li>The Name of the Rose</li>
-          <li>One Hundred Years of Solitude</li>
-          <li>Cry, the Beloved Country</li>
+          <li style={{background: theme.ui}}>The Name of the Rose</li>
+          <li style={{background: theme.ui}}>One Hundred Years of Solitude</li>
+          <li style={{background: theme.ui}}>Cry, the Beloved Country</li>
         </ul>
       </div>
     );
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -18,7 +18,7 @@ class ThemeContextProvider extends Component {
 
   render() { 
     return (
-      <ThemeContext.Provider value={this.state}>
+      <ThemeContext.Provider value={{...this.state}}>
         {this.props.children}
       </ThemeContext.Provider>
     );
