66cae6fe6fda274a413003d10723bbba6baa0cd3
Ankush Thakur <ankush.thakur53@gmail.com>
2020-07-21 13:31:22 +0530

    create ThemeContextProvider

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,27 @@
+import React, { createContext } from 'react';
+export const ThemeContext = createContext();
+
+class ThemeContextProvider extends Component {
+  state = { 
+    isLightTheme: true,
+    light: {
+      text: '#555',
+      ui: '#DDD',
+      bg: '#EEE'
+    },
+    dark: {
+      text: '#DDD',
+      ui: '#333',
+      bg: '#555'
+    }
+  };
+
+  render() { 
+    return (
+      <ThemeContext.Provider value={this.state}>
+      </ThemeContext.Provider>
+    );
+  }
+}
+ 
+export default ThemeContextProvider;
