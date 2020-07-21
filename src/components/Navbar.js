667a8245e4eaaffc8d406d86bbc1568a6487f17f
Ankush Thakur <ankush.thakur53@gmail.com>
2020-07-21 13:58:29 +0530

    Change Navbar to use ContextConsumer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-  static contextType = ThemeContext;
-
   render() {
-    const {isLightTheme, dark, light} = this.context;
-    const theme = isLightTheme ? light : dark;
-
-    return (  
-      <nav style={{background: theme.ui, color: theme.text}}>
-        <h1>Book-Reading List</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+    return (
+      <ThemeContext.Consumer>
+        {
+          context => {
+            const {isLightTheme, dark, light} = context;
+            const theme = isLightTheme ? light : dark;
+            return (
+              <nav style={{background: theme.ui, color: theme.text}}>
+                <h1>Book-Reading List</h1>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            );
+          }
+        }
+      </ThemeContext.Consumer>
     );
   }
 }
